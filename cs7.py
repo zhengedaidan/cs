293f85ed1090e5178a77efab20d400210c4993a6
ls1 <lstjkdy@163.com>
2018-05-17 13:36:04 +0800

    aa

--- cs7.py
+++ cs7.py
@@ -1,4 +1,17 @@
-if 1==1:
-    print(666)
-    print(777)
-    print("masa")
+from datetime import datetime,date,timedelta
+import time
+s="2018:08:08 08:08:08"
+t=datetime.strptime(s,"%Y:%m:%d %H:%M:%S")
+print(t)
+
+t1=t+timedelta(days=6)
+print(t1)
+
+tu=datetime.utcnow()
+print(tu)
+
+tc=time.strptime(s, "%Y:%m:%d %H:%M:%S")
+print(list(tc))
+print(type(tc))
+print(tc)
+print(100*"*")
--- cs8.py
+++ cs8.py
@@ -1,3 +1,12 @@
 # -*- coding:utf-8 -*-
-print(111)
-print(222)
+class A:
+    def show(self):
+        print("a")
+class B(A):
+    def show(self):
+        print("b")
+obj=B()
+print(obj.__class__)
+obj.__class__=A
+obj.show()
+print(obj)
